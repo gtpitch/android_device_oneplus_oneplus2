4f7070274498cb2d1d67db705291af0907f4a557
gtpitch <bmontagu@gmail.com>
2017-09-02 12:13:50 -0400

    Revert "oneplus2: HAL3"
    This reverts commit ea44f831f2c8428ae6e804443d5c69e489d65018.

--- device.mk
+++ device.mk
@@ -103,14 +103,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.msm8994 \
     libshim_camera \
-    sensors.hal.tof \
-    mm-qcamera-app \
-    libmm-qcamera \
+    libshim_ims-camera \
     sensors.hal.tof \
     Snap
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.d/10opcamsanitize.sh:system/etc/init.d/10opcamsanitize.sh
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
